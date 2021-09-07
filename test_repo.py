05ab107385a85ea155fe2e83c8d7ca0aeba03a4f
Lenz Fiedler <l.fiedler@hzdr.de>
2021-09-07 17:55:36 +0200

    Modified repo tester

--- test_repo.py
+++ test_repo.py
@@ -1,7 +1,9 @@
 import numpy as np
 
+# This file is only for to check whether the linking to MALA worked.
+
 target_array = [1,2,3,4]
 target_array = np.array(target_array)
 saved_array = np.load("linking_tester.npy")
-if np.abs(np.sum(target_array-saved_array)) > 0.0000000000001:
+if not np.isclose(target_array, saved_array, atol=1e-20).all():
     raise Exception("Something is wrong with the repo.")
